bcdfc956a9b384310053f35bd8fdec7164594058
200615 <200615@e0601-21.JC-21.local>
2022-02-07 10:44:01 +0900

    otokadai05の再提出

--- src/Otokadai05.java
+++ src/Otokadai05.java
@@ -21,25 +21,19 @@ public class Otokadai05  {
 		oto2.powerOn();
 		while(true) {
 			oto1.lightGreen();
-			oto2.lightOff();
 			oto1.sleep();
-			oto2.sleep();
 			oto1.lightYellow();
 			oto1.sleep();
-			oto2.sleep();
 			oto1.lightRed();
 			oto1.sleep();
-			oto2.sleep();
 			oto1.lightOff();
 			oto2.lightGreen();
-			oto1.sleep();
 			oto2.sleep();
 			oto2.lightYellow();
-			oto1.sleep();
 			oto2.sleep();
 			oto2.lightRed();
-			oto1.sleep();
 			oto2.sleep();
+			oto2.lightOff();
 		}
 
 	}
